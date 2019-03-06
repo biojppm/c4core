6685e958ed7554f832899469a07836d0ad73ac53
Joao Paulo Magalhaes <dev@jpmag.me>
2019-03-06 23:49:08 +0000

    [ci skip] roll back the lambda technique for the asserts; will need to create a
    compile-time assert usable in constexpr functions

--- src/c4/error.hpp
+++ src/c4/error.hpp
@@ -284,13 +284,14 @@ struct srcloc
  * builds.
  * @see C4_ASSERT
  * @ingroup error_checking
+ *
+ * @todo add constexpr-compatible compile-time assert:
+ * https://akrzemi1.wordpress.com/2017/05/18/asserts-in-constexpr-functions/
  */
-#define C4_CHECK(cond)                                  \
-    if(C4_UNLIKELY(!(cond)))                            \
-    {                                                   \
-        /* the lambda is required for allowing use in C++11 constexpr functions \
-         * see: https://akrzemi1.wordpress.com/2017/05/18/asserts-in-constexpr-functions/ */\
-        []{ C4_ERROR("check failed: %s", #cond); }();   \
+#define C4_CHECK(cond)                          \
+    if(C4_UNLIKELY(!(cond)))                    \
+    {                                           \
+        C4_ERROR("check failed: %s", #cond);    \
     }
 
 /** like C4_CHECK(), and additionally log a printf-style message.
