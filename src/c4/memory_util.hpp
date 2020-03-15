7ff2981c64985078a5261d05bf0dc2e62956656d
Joao Paulo Magalhaes <dev@jpmag.me>
2020-03-16 00:18:40 +0100

    let msb() and lsb() accept 0 and return -1

--- src/c4/memory_util.hpp
+++ src/c4/memory_util.hpp
@@ -105,14 +105,14 @@ struct lsb11
 template<class I>
 C4_CONSTEXPR14 I msb(I v)
 {
-    if(!v) return 0;
+    if(!v) return static_cast<I>(-1);
     I b = 0;
     while(v != 0)
     {
         v >>= 1;
         ++b;
     }
-    return b;
+    return b-1;
 }
 
 namespace detail {
@@ -130,7 +130,7 @@ template<class I, I val, I num_bits>
 struct _msb11<I, val, num_bits, true>
 {
     static_assert(val == 0, "bad implementation");
-    enum : I { num = num_bits };
+    enum : I { num = num_bits-1 };
 };
 
 } // namespace detail
