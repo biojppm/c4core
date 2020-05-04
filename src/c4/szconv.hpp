467c8aeff0385924d1a709dbed14ef34bcabe48e
Joao Paulo Magalhaes <dev@jpmag.me>
2020-05-04 09:31:07 +0200

    formatting only

--- src/c4/szconv.hpp
+++ src/c4/szconv.hpp
@@ -11,10 +11,10 @@
 C4_BEGIN_NAMESPACE(c4)
 
 /** @todo this would be so much easier with calls to numeric_limits::max()... */
-template< class SizeOut, class SizeIn >
+template<class SizeOut, class SizeIn>
 struct is_narrower_size : std::conditional
 <
-   (std::is_signed< SizeOut >::value == std::is_signed< SizeIn >::value)
+   (std::is_signed<SizeOut>::value == std::is_signed<SizeIn>::value)
    ?
    (sizeof(SizeOut) < sizeof(SizeIn))
    :
@@ -24,22 +24,22 @@ struct is_narrower_size : std::conditional
        (
            (sizeof(SizeOut) == sizeof(SizeIn))
            &&
-           (std::is_signed< SizeOut >::value && ( ! std::is_signed< SizeIn >::value))
+           (std::is_signed<SizeOut>::value && std::is_unsigned<SizeIn>::value)
        )
    ),
    std::true_type,
    std::false_type
 >::type
 {
-    static_assert(std::is_integral< SizeIn  >::value, "must be integral type");
-    static_assert(std::is_integral< SizeOut >::value, "must be integral type");
+    static_assert(std::is_integral<SizeIn >::value, "must be integral type");
+    static_assert(std::is_integral<SizeOut>::value, "must be integral type");
 };
 
 
 /** when SizeOut is wider than SizeIn, assignment can occur without reservations */
-template< class SizeOut, class SizeIn >
+template<class SizeOut, class SizeIn>
 C4_ALWAYS_INLINE
-typename std::enable_if< ! is_narrower_size< SizeOut, SizeIn >::value, SizeOut >::type
+typename std::enable_if< ! is_narrower_size<SizeOut, SizeIn>::value, SizeOut>::type
 szconv(SizeIn sz) noexcept
 {
     return sz;
@@ -48,13 +48,13 @@ szconv(SizeIn sz) noexcept
 /** when SizeOut is narrower than SizeIn, narrowing will occur, so we check
  * for overflow. Note that this check is done only if C4_XASSERT is enabled.
  * @see C4_XASSERT */
-template< class SizeOut, class SizeIn >
+template<class SizeOut, class SizeIn>
 C4_ALWAYS_INLINE
-typename std::enable_if< is_narrower_size< SizeOut, SizeIn >::value, SizeOut >::type
+typename std::enable_if<is_narrower_size<SizeOut, SizeIn>::value, SizeOut>::type
 szconv(SizeIn sz) C4_NOEXCEPT_X
 {
     C4_XASSERT(sz >= 0);
-    C4_XASSERT_MSG((SizeIn)sz <= (SizeIn)std::numeric_limits< SizeOut >::max(), "size conversion overflow: in=%zu", (size_t)sz);
+    C4_XASSERT_MSG((SizeIn)sz <= (SizeIn)std::numeric_limits<SizeOut>::max(), "size conversion overflow: in=%zu", (size_t)sz);
     SizeOut szo = (SizeOut)sz;
     return szo;
 }
