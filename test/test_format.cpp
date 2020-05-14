73c1497b5cd4f6a4c6d9d8eeaebf50a371e14965
Joao Paulo Magalhaes <dev@jpmag.me>
2020-05-14 02:16:38 +0200

    fix VS compile errors

--- test/test_format.cpp
+++ test/test_format.cpp
@@ -62,7 +62,7 @@ TEST_CASE("to_chars.fmt_basic")
     CHECK_EQ(to_chars_sub(buf, fmt::hex( int8_t(0x7f))), "0x7f");
     CHECK_EQ(to_chars_sub(buf, fmt::hex(uint8_t(0xff))), "0xff");
 
-    return; // FIXME readfloat
+    #ifdef FIXME_READFLOAT
     float f = 256.064f;
     test_to_chars_fmt_basic<float>(f, 0, "256", 256.f, "3e+02", 300.f);
     test_to_chars_fmt_basic<float>(f, 1, "256.1", 256.1f, "2.6e+02", 260.f);
@@ -78,6 +78,7 @@ TEST_CASE("to_chars.fmt_basic")
     test_to_chars_fmt_basic<double>(d, 3, "256.064", 256.064, "2.561e+02", 256.1);
     test_to_chars_fmt_basic<double>(d, 4, "256.0640", 256.0640, "2.5606e+02", 256.06);
     test_to_chars_fmt_basic<double>(d, 5, "256.06400", 256.06400, "2.56064e+02", 256.064);
+    #endif
 }
 
 
