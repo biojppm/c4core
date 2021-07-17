ca24d655293af752e91d3a1ecd51d86d09a89fe3
Joao Paulo Magalhaes <dev@jpmag.me>
2021-07-17 12:36:55 +0100

    [impl] is_debugger_attached(): MacOSX version

--- src/c4/error.cpp
+++ src/c4/error.cpp
@@ -17,6 +17,12 @@
 #   include <cstring>
 #   include <fcntl.h>
 #   include <unistd.h>
+#elif defined(C4_MACOS) || defined(C4_IOS)
+#   include <assert.h>
+#   include <stdbool.h>
+#   include <sys/types.h>
+#   include <unistd.h>
+#   include <sys/sysctl.h>
 #endif
 
 #if defined(C4_EXCEPTIONS_ENABLED) && defined(C4_ERROR_THROWS_EXCEPTION)
@@ -173,8 +179,37 @@ bool is_debugger_attached()
     return (sceDbgIsDebuggerAttached() != 0);
 #elif defined(C4_XBOX) || (defined(C4_WIN) && defined(C4_MSVC))
     return IsDebuggerPresent() != 0;
+#elif defined(C4_MACOS) || defined(C4_IOS)
+    // https://stackoverflow.com/questions/2200277/detecting-debugger-on-mac-os-x
+    // Returns true if the current process is being debugged (either
+    // running under the debugger or has a debugger attached post facto).
+    int                 junk;
+    int                 mib[4];
+    struct kinfo_proc   info;
+    size_t              size;
+
+    // Initialize the flags so that, if sysctl fails for some bizarre
+    // reason, we get a predictable result.
+
+    info.kp_proc.p_flag = 0;
+
+    // Initialize mib, which tells sysctl the info we want, in this case
+    // we're looking for information about a specific process ID.
+
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_PROC;
+    mib[2] = KERN_PROC_PID;
+    mib[3] = getpid();
+
+    // Call sysctl.
+
+    size = sizeof(info);
+    junk = sysctl(mib, sizeof(mib) / sizeof(*mib), &info, &size, NULL, 0);
+    assert(junk == 0);
+
+    // We're being debugged if the P_TRACED flag is set.
+    return ((info.kp_proc.p_flag & P_TRACED) != 0);
 #else
-    C4_NOT_IMPLEMENTED();
     return false;
 #endif
 } // is_debugger_attached()
