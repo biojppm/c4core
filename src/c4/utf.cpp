3fbc0317146de944285f818ffbe32dee3785385b
Joao Paulo Magalhaes <dev@jpmag.me>
2025-01-18 21:24:11 +0000

    utf: add utilities for Byte Order Marks

--- src/c4/utf.cpp
+++ src/c4/utf.cpp
@@ -55,6 +55,59 @@ substr decode_code_point(substr out, csubstr code_point)
     return out.first(ret);
 }
 
+size_t first_non_bom(csubstr s)
+{
+    #define c4check2_(s, c0, c1)         ((s).len >= 2) && (((s).str[0] == (c0)) && ((s).str[1] == (c1)))
+    #define c4check3_(s, c0, c1, c2)     ((s).len >= 3) && (((s).str[0] == (c0)) && ((s).str[1] == (c1)) && ((s).str[2] == (c2)))
+    #define c4check4_(s, c0, c1, c2, c3) ((s).len >= 4) && (((s).str[0] == (c0)) && ((s).str[1] == (c1)) && ((s).str[2] == (c2)) && ((s).str[3] == (c3)))
+    // see https://en.wikipedia.org/wiki/Byte_order_mark#Byte-order_marks_by_encoding
+    if(s.len < 2u)
+        return false;
+    else if(c4check3_(s, '\xef', '\xbb', '\xbf')) // UTF-8
+        return 3u;
+    else if(c4check4_(s, '\x00', '\x00', '\xfe', '\xff')) // UTF-32BE
+        return 4u;
+    else if(c4check4_(s, '\xff', '\xfe', '\x00', '\x00')) // UTF-32LE
+        return 4u;
+    else if(c4check2_(s, '\xfe', '\xff')) // UTF-16BE
+        return 2u;
+    else if(c4check2_(s, '\xff', '\xfe')) // UTF-16BE
+        return 2u;
+    else if(c4check3_(s, '\x2b', '\x2f', '\x76')) // UTF-7
+        return 3u;
+    else if(c4check3_(s, '\xf7', '\x64', '\x4c')) // UTF-1
+        return 3u;
+    else if(c4check4_(s, '\xdd', '\x73', '\x66', '\x73')) // UTF-EBCDIC
+        return 4u;
+    else if(c4check3_(s, '\x0e', '\xfe', '\xff')) // SCSU
+        return 3u;
+    else if(c4check3_(s, '\xfb', '\xee', '\x28')) // BOCU-1
+        return 3u;
+    else if(c4check4_(s, '\x84', '\x31', '\x95', '\x33')) // GB18030
+        return 4u;
+    return 0u;
+    #undef c4check2_
+    #undef c4check3_
+    #undef c4check4_
+}
+
+substr get_bom(substr s)
+{
+    return s.first(first_non_bom(s));
+}
+csubstr get_bom(csubstr s)
+{
+    return s.first(first_non_bom(s));
+}
+substr skip_bom(substr s)
+{
+    return s.sub(first_non_bom(s));
+}
+csubstr skip_bom(csubstr s)
+{
+    return s.sub(first_non_bom(s));
+}
+
 C4_SUPPRESS_WARNING_GCC_CLANG_POP
 
 } // namespace c4
